6af2f81830e63ca4f77dada938d2b2a46349f3f1
Nazmul Alam Mithu <mithu.uap@gmail.com>
2018-10-01 06:31:09 +0600

    Initial commit

---
+++ photo.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ *  PHOTO.PHP
+ *
+ *  The page shows the average number of likes of photos you are tagged in
+ *  It uses fql to do so.
+ *
+ */
+
+require_once 'fb/auth.php';
+
+// If there is no logged in user, redirect to login.php
+if(!$user)
+	header("Location: login.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css" />
+</head>
+<body>
+	<div class="container" style="margin-top: 20px">
+	<?php
+		$average_like = 0;
+	
+		echo
+			"<div class='page-header'>
+				<h1>
+					On average, ".round($average_like, 2)." people like photos you are tagged in
+					<small>Congratulations</small>
+				</h1>
+			</div>";
+	
+	?>
+	
+		<h3>Photos you're in that have the most likes:</h3><br />
+		
+	</div>
+</body>
+</html>
